09987f0bb25d56282c816e933624e71b5a121c95
Bruno C <mail@brunocartier.fr>
2015-11-18 16:00:40 +0100

    Routes, my friend, routes \o/

--- js/main.js
+++ js/main.js
@@ -5,8 +5,30 @@
     'use strict';
 
     var app = ng.module('SondeReader', [
+        'ui.router',
         'ngMaterial'
     ]);
+
+    app.config(['$stateProvider', '$urlRouterProvider', function (
+        $stateProvider,
+        $urlRouterProvider
+    ) {
+        console.log('setting routes');
+
+        $stateProvider.state('home', {
+            url: '/home',
+            controller: 'TestController',
+            templateUrl: 'partials/home.html'
+        });
+
+        $stateProvider.state('settings', {
+            url: '/settings',
+            controller: 'TestController',
+            templateUrl: 'partials/settings.html'
+        });
+
+        $urlRouterProvider.otherwise('/home');
+    }]);
 
     app.controller('TestController', ['$scope', function (self) {
         self.hello = 'World';
